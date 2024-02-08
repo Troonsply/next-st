d7e539be92c2cd578fae5b004a5c665b2ea06454
troonsply <troonsply@gmail.com>
2024-02-08 13:47:48 +0300

    commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function About() {
+  return (
+      <div>
+        <h1>Страница про нас</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      </div>
+  );
+}
